008629c790b15e7da1e52f334dbd94b5c067448d
Tejastatestvagrant <139124793+Tejastatestvagrant@users.noreply.github.com>
2023-07-26 13:04:35 +0530

    read and write from database

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
+import { promisify } from 'util';
 import Note from './src/Note.js'
-
+import fs, { writeFileSync } from 'fs';
 function welcomeMessage() {
     console.log("Welcome to the Note-Taking Application!");
   }
@@ -9,16 +10,16 @@ function welcomeMessage() {
   let noteList= [];
 
 
-  let createNote=(title,content)=>{
-    let newNote=new Note(title,content);
-    noteList.push(newNote);
+  // let createNote=(title,content)=>{
+  //   let newNote=new Note(title,content);
+  //   noteList.push(newNote);
 
-  }
+  // }
 
  
-  createNote('First Note', 'This is the first note.');
-  createNote('Second Note', 'This is the second note.');
-  createNote('Third Note', 'This is the third note.');
+  // createNote('First Note', 'This is the first note.');
+  // createNote('Second Note', 'This is the second note.');
+  // createNote('Third Note', 'This is the third note.');
 
   
   const displayAllNotes= ()=>{for (const note of noteList) {
@@ -60,8 +61,55 @@ function welcomeMessage() {
 
   }
 
+  //  const writeDataToFile= async(data,filename)=>{
+       
+  //   const jsonData= JSON.stringify(data)
+
+  // fs.writeFile(filename,jsonData,'utf-8',(err)=>{
+  //   if(err)
+  //   {
+  //     console.log(`error occured during writing file`);
+  //   }
+  //   else{
+  //     console.log(`Successfully updated file `);
+
+  //   }
+  // })
 
 
-  deleteNote('Second Note');
-  editNode('First Note' ,'updated first version');
-  displayAllNotes();
+  const savaFile=async(data,file)=>{
+
+    try{const writeFileAsync = promisify(fs.writeFile);
+    const jsonData = JSON.stringify(data);
+    await writeFileAsync(file,jsonData,'utf-8');
+    console.log("Successfully updated");}
+    catch(err)
+    {
+      console.log("Error Occurred " +err);
+    }
+
+  }
+
+  const loadData=async(file)=>{
+
+    const readFileAsync=promisify(fs.readFile);
+
+    const loadedData = await readFileAsync(file,'utf-8');
+    const datas= await JSON.parse(loadedData);
+
+   
+    noteList=datas.map(data=>new Note(data.title,data.content));
+  }
+
+
+
+
+ await loadData('./src/Database.json');
+  console.table(noteList);
+  
+
+
+
+  // deleteNote('Second Note');
+  // editNode('First Note' ,'updated first version');
+  // savaFile(noteList,'./src/Database.json')
